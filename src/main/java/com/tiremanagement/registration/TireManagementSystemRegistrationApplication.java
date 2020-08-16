5a225f4711193f1664cc1cae7d0f2ddf8109fa2d
prakash-gangadharan <prakash08.g@gmail.com>
2020-08-16 19:26:26 +0530

    initial commit for TireManagementSystem

---
+++ src/main/java/com/tiremanagement/registration/TireManagementSystemRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.tiremanagement.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireManagementSystemRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TireManagementSystemRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiremanagement/registration/TireManagementSystemRegistration/TireManagementSystemRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiremanagement.registration.TireManagementSystemRegistration;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireManagementSystemRegistrationApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
