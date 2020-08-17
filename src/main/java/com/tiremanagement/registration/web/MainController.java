143eac2a82ea22b03d902651316c53ae8a000ea0
prakash-gangadharan <prakash08.g@gmail.com>
2020-08-17 22:16:45 +0530

    update user name

--- src/main/java/com/tiremanagement/registration/web/MainController.java
+++ src/main/java/com/tiremanagement/registration/web/MainController.java
@@ -1,11 +1,16 @@
 package com.tiremanagement.registration.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tiremanagement.registration.entity.User;
 import com.tiremanagement.registration.repository.UserRepository;
@@ -30,13 +35,24 @@ public class MainController {
     public String userIndex() {
         return "user/index";
     }
-    
-    @GetMapping("/dashboardUser")
-    public String dashboardPageList(Model model, @AuthenticationPrincipal UserDetails currentUser ) {
-    User user = (User) userRepository.findUserByUsername(currentUser.getUsername());
-        model.addAttribute("currentStudent", user);
 
-        return "dashboardUser";
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+        
+        userRepository.updateUsername(id, user.getUsername());
+        model.addAttribute("user", userRepository.findById(id));
+        return "index";
     }
     
 }
